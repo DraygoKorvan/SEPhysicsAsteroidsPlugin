f43bfe3e3b0a96e391340a5b4b8a7d739528be5a
DraygoKorvan <draygokorvan@ps-roughriders.com>
2014-07-13 23:10:54 -0500

    -final for this branch only log when debugging is enabled

--- PhysicsAsteroidsPlugin/Core.cs
+++ PhysicsAsteroidsPlugin/Core.cs
@@ -61,13 +61,7 @@ namespace PhysicsAsteroidsPlugin
 			MyPositionAndOrientation position;
 			Vector3 pos;
 			Vector3 velocity;
-			//Console.WriteLine("Update()...");
-//			Type temp_o_type;
-//				MyObjectBuilder_FloatingObject tempobject;
-//			MyObjectBuilder_Ore tempore = new MyObjectBuilder_Ore();
-//			MyObjectBuilder_InventoryItem tempitem = new MyObjectBuilder_InventoryItem();
-//			tempore.SetDefaultProperties();
-//			FloatingObject physicsmeteor;
+
 
 			List<ulong> connectedPlayers = ServerNetworkManager.Instance.GetConnectedPlayers();
 			List<BaseEntity> entityList = SectorObjectManager.Instance.GetTypedInternalData<BaseEntity>();
@@ -79,63 +73,22 @@ namespace PhysicsAsteroidsPlugin
 				{
 					if (!sectorObject.IsDisposed)
 					{
-						Console.WriteLine("Meteor Detected: " + sectorObject.Name + " EntityID:" + sectorObject.EntityId.ToString());
-						//deleting meteor
-						position = sectorObject.PositionAndOrientation;
-						pos = sectorObject.Position;
-						velocity = sectorObject.LinearVelocity;
-						sectorObject.Dispose();
-						//Console.WriteLine("Meteor Properties: " + sectorObject.Name + " EntityID:" + sectorObject.EntityId.ToString() + " Velocity: " + velocity.ToString() + " Position: " + pos.ToString());
-						Console.WriteLine("Meteor Deleted: " + sectorObject.Name + " EntityID:" + sectorObject.EntityId.ToString());
-						//if there are not enough players connected, abort the shower
-						if (connectedPlayers.Count > 0)
+						if (connectedPlayers.Count == 0)
 						{
-							/*try
+							//if there are not enough players connected, abort the shower
+							//Console.WriteLine("Meteor Detected: " + sectorObject.Name + " EntityID:" + sectorObject.EntityId.ToString());
+							//deleting meteor
+							position = sectorObject.PositionAndOrientation;
+							pos = sectorObject.Position;
+							velocity = sectorObject.LinearVelocity;
+							sectorObject.Dispose();
+							//Console.WriteLine("Meteor Properties: " + sectorObject.Name + " EntityID:" + sectorObject.EntityId.ToString() + " Velocity: " + velocity.ToString() + " Position: " + pos.ToString());
+							if (SandboxGameAssemblyWrapper.IsDebugging)
 							{
-								temp_o_type = SandboxGameAssemblyWrapper.Instance.GameAssembly.GetType("MyObjectBuilder.FloatingObject");
-								Object BackingObject = Activator.CreateInstance(temp_o_type);
-								MethodInfo initMethod = BackingObject.GetType().GetMethod("Init", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-								initMethod.Invoke(BackingObject, new object[] { });								
+								Console.WriteLine("Meteor Deleted: " + sectorObject.Name + " EntityID:" + sectorObject.EntityId.ToString());
+								LogManager.APILog.WriteLine("Meteor Deleted: " + sectorObject.Name + " EntityID:" + sectorObject.EntityId.ToString());
 							}
-							catch (Exception ex)
-							{
-								Console.WriteLine("Exception: " + ex.ToString());
-							}*/
-							/*try
-							{
-								m_ore_fctr = m_gen.NextDouble();
-
-								tempobject = new MyObjectBuilder_FloatingObject();
-								tempobject.PositionAndOrientation = position;
-								tempobject.Name = "Stone";
-								tempitem.SetDefaultProperties();
-								tempitem.AmountDecimal = Math.Round((decimal)(m_ore_amt * m_ore_fctr));
-								tempitem.ItemId = 0;
-								tempitem.PhysicalContent = new MyObjectBuilder_PhysicalObject();
-								tempitem.PhysicalContent.ChangeType(tempore.TypeId, "Stone");
-								tempobject.Item = tempitem;
-								
-								try
-								{
-									//spawn in new floating object
-									physicsmeteor = new FloatingObject(tempobject);
-									physicsmeteor.EntityId = physicsmeteor.GenerateEntityId();
-									physicsmeteor.LinearVelocity = velocity;
-									physicsmeteor.MaxLinearVelocity = 104.7F;
-									SectorObjectManager.Instance.AddEntity(physicsmeteor);
-									Console.WriteLine("Floating Object Spawned: " + physicsmeteor.EntityId.ToString());
-								}
-								catch (Exception ex)
-								{
-									Console.WriteLine("Exception: floatingobject " + ex.ToString());
-									throw;
-								}
-							}
-							catch (Exception ex)
-							{
-								Console.WriteLine("Exception: myobjectbuilder_floatingobject " + ex.ToString());
-							}
-							*/
+							
 						}
 					}
 				}
